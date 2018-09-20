49231479fd1e73593ef332bd4888495feb9d3012
pomber <pombopombopombo@gmail.com>
2018-09-20 11:30:30 -0400

    Deploy to now

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-module.exports = req => "Welcome to Micro";
+require("dotenv").config();
+const fetch = require("node-fetch");
+
+module.exports = async req => {
+  const files = [
+    { file: "index.html", data: "<html><body>INDEX</body></html>" }
+  ];
+
+  const token = process.env.NOW_TOKEN;
+
+  const response = await fetch("https://api.zeit.co/v3/now/deployments", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${token}`
+    },
+    body: JSON.stringify({
+      public: true,
+      name: "publish-test",
+      deploymentType: "STATIC",
+      files
+    })
+  });
+  const deployment = await response.json();
+  console.log(deployment);
+
+  return deployment.url;
+};
